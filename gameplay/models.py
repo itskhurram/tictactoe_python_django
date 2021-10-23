1e53673ca6790abb7c8fe1bdc3448ff0beb248f3
itskhurram <itskhurrams@outlook.com>
2021-10-24 01:28:19 +0500

    model issue but checkedin

--- gameplay/models.py
+++ gameplay/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+STATUS_CHOICES = (('F', 'First Player Move'), ('S', 'Second Player Move'),
+                  ('W', 'First Player Win'), ('L', 'Second Player Win'),
+                  ('D', 'Draw'))
+
 
 # Create your models here.
 class Game(models.Model):
-    first_player = models.ForeignKey(User,
-                                     related_name="game_first_player",
-                                     on_delete=models.CASCADE),
-    second_player = models.ForeignKey(User,
-                                      related_name="game_second_player",
-                                      on_delete=models.CASCADE),
+    first_player = models.ForeignKey(User, on_delete=models.CASCADE),
+    second_player = models.ForeignKey(User, on_delete=models.CASCADE),
     start_time = models.DateTimeField(auto_now_add=True),
     last_active = models.DateTimeField(auto_now_add=True),
-    status = models.CharField(max_length=1, default='F')
+    status = models.CharField(max_length=1,
+                              default='F',
+                              choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return "{0} vs {1}".format(self.first_player, self.second_player)
 
 
 class Move(models.Model):
